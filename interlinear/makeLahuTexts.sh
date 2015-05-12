0ad581b11779c1cadfb923b8ede2bc9bcd652b1a
Chundra Cathcart <chundracathcart@Chundra-Cathcarts-MacBook.local>
2015-05-12 12:02:29 -0700

    nonstop version of this script

--- interlinear/makeLahuTexts.sh
+++ interlinear/makeLahuTexts.sh
@@ -19,11 +19,11 @@ python ../combiner.py ../lahutextstoc.txt
 sed -e '/% insert includes here/r./includes.tex' lahuTemplate.tex > ${texfile}.tex
 #
 echo Compiling LaTeX file "${texfile}.tex", step 1 of 3, timestamp: `date`
-xelatex ${texfile} > ${texfile}.stdout.log
+xelatex -interaction nonstopmode ${texfile} > ${texfile}.stdout.log
 echo Compiling LaTeX file "${texfile}.tex", step 2 of 3, timestamp: `date`
-xelatex ${texfile} >> ${texfile}.stdout.log
+xelatex -interaction nonstopmode ${texfile} >> ${texfile}.stdout.log
 echo Compiling LaTeX file "${texfile}.tex", step 3 of 3, timestamp: `date`
-xelatex ${texfile} >> ${texfile}.stdout.log
+xelatex -interaction nonstopmode ${texfile} >> ${texfile}.stdout.log
 echo Done. `date`
 echo Output is tex/${texfile}.tex, logs are in ${texfile}.stdout.log
 echo "=============================================================================="
