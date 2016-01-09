9caf91b746d3ba09cda47f41601ee44e7951390a
John Lowe <jblowe@berkeley.edu>
2016-01-09 10:08:23 -0800

    refactored this script to handle footnotes better

--- interlinear/editTeX.py
+++ interlinear/editTeX.py
@@ -7,25 +7,61 @@ footHash = {}
 
 footnote_number_pattern = re.compile(r'^\[(\d+)\] *(.*)')
 
-for line in fileinput.input():
+def fixLine(line):
+    
     line = line.strip()
-    line = line.replace('\texttt{"}', '"')
+    line = re.sub(r'\\texttt\{(.*?)\}', r'\1', line);
     line = re.sub(r'\@(.*?)\@', '_{\1}', line);
     line = line.replace('\Huge{}', '');
     line = line.replace('\huge{}', '');
-    if 'vspace' in line: continue
-    if 'parindent' in line: continue
-    if 'baselineskip' in line: continue
+
+    return line
+
+def test4skip(line):
+    
+    if 'vspace' in line: return True
+    if 'parindent' in line: return True
+    if 'baselineskip' in line: return True
+
+    return False
+
+def addnote(footnote,footHash):
+    
+    textnumbermatch = footnote_number_pattern.search(footnote)
+    if textnumbermatch:
+        footnote_number = textnumbermatch.group(1)
+        rest_of_line = textnumbermatch.group(2)
+        footHash[footnote_number] = rest_of_line.strip()
+
+inputLines = []
+for line in fileinput.input():
+    line = fixLine(line)
+    if test4skip(line): continue
+    inputLines.append(line.strip())
+
+for pointer, line in enumerate(inputLines):
     if 'Footnote' in line: footnoteson = True
     if re.match(r'^\[\d+\]',line): footnoteson = True
     if footnoteson:
-        textnumbermatch = footnote_number_pattern.search(line)
-        if textnumbermatch:
-            footnote_number = textnumbermatch.group(1)
-            rest_of_line = textnumbermatch.group(2)
-            footHash[footnote_number] = rest_of_line
+        break
+    lineArray.append(line)
+
+inputLines = inputLines[pointer:]
+
+# process footnotes
+footnote = ''
+for line in inputLines:
+
+    if re.match(r'^\[\d+\]',line):
+
+        addnote(footnote,footHash)
+        footnote = line
+        
     else:
-        lineArray.append(line)
+        footnote += ' ' + line
+
+addnote(footnote,footHash)
+    
 for line in lineArray:
     # move footnote mark outside period: xxx xx[99]. ->  xxx xx.[99]
     line = re.sub(r'(\[\d+\])\.', r'\.\1', line)
