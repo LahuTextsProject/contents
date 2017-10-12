a71b197ae0b4e7fecf710193cc604301071e104f
Charles Zhang <charleszhang99@yahoo.com>
2017-10-11 17:28:10 -0700

    Don't use sed for what a flag to rtf2latex can do

--- interlinear/texRTFs.sh
+++ interlinear/texRTFs.sh
@@ -7,12 +7,12 @@ mkdir ../t2
 cp ../rtfs/*.rtf ../t2
 for f in `ls ../t2/*.rtf`
 do
-    rtf2latex2e -n $f
+    rtf2latex2e -nf $f
 done
 for f in `ls ../t2/*.tex`
 do
     r=$(basename "$f")
     echo "editing $r"
-    sed -n '/begin/,$p' $f | tail -n +2 | sed -n '/end{document}/q;p' | grep -v 'newpage' | python2 editTeX.py > ../transTeX/$r
+    grep -v 'newpage' $f | python2 editTeX.py > ../transTeX/$r
     #rm $f
 done
