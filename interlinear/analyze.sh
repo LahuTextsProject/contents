f0c1b6aa7a7ad7353ce815b1e1c97d977570a96c
Charles Zhang <charleszhang99@yahoo.com>
2017-10-23 11:37:08 -0700

    Create a better abbreviations listing

--- interlinear/analyze.sh
+++ interlinear/analyze.sh
@@ -3,5 +3,5 @@ grep "txt" lahutexts.xml | grep -v segnum | perl -pe 's/^.*?<word><item type=\"t
 
 # TODO Exclude Black | Lahu | translation from file somehow
 perl -ne 'next if /segnum/;s/\r//; print "\n" if /<\/morph>/; chomp; if (/(txt|gls|msa)/) {s/^.*?lang=\"(?:en|lhu)\">(.*)<\/item>.*$/\1/;print ; print "\t";}' lahutexts.xml | grep -v segnum > elements.csv
-perl -ne 'next if /segnum/;s/\r//; print "\n" if /<\/morph>/; chomp; if (/msa/) {s/^.*?lang=\"(?:en|lhu)\">(.*)<\/item>.*$/\1/;print ; print "\t";}' lahutexts.xml | grep -v segnum | uniq -c > abbreviations.csv
+perl -ne 'next if /segnum/;s/\r//; print "\n" if /<\/morph>/; chomp; if (/msa/) {s/^.*?lang=\"(?:en|lhu)\">(.*)<\/item>.*$/\1/;print ; print "\t";}' lahutexts.xml | grep -v segnum | sort | uniq > abbreviations.csv
 sort elements.csv | uniq -c | sort -rn | perl -pe 's/^ *(\d+) /\1\t/' > triples.csv
