11bb7a322cf497628ae2f76110636a17c8a0fa0f
John Lowe <jblowe@berkeley.edu>
2021-01-20 10:37:15 -0800

    make the makeLahuTexts.sh script a bit less unwieldy

--- interlinear/makeLahuTexts.sh
+++ interlinear/makeLahuTexts.sh
@@ -3,6 +3,8 @@ if [[ `pwd` != */interlinear ]]; then
   exit
 fi
 texfile="WindowsForgottenWorld"
+xml_file=$1
+mkdir -p ../t2
 echo
 echo "=============================================================================="
 echo Starting run `date`
@@ -14,14 +16,18 @@ case $2 in baptist|chinese|matisovian)
       exit 1 ;;
 esac
 echo Making symbolic link lahutexts.xml. source is: $1
-ln -sf $1 lahutexts.xml
+cp $xml_file lahutexts.xml
 echo Converting RTFs to LaTeX
 ./texRTFs.sh
 echo Making music scores
-# for charles' linux:
-# lilypond --ps Lahu_tune.ly
-# for jb's mac:
-/Applications/LilyPond.app/Contents/Resources/bin/lilypond --ps Lahu_tune.ly
+if [[ `uname` == "Darwin" ]]
+then
+   # for jb's mac:
+   /Applications/LilyPond.app/Contents/Resources/bin/lilypond --ps Lahu_tune.ly
+else
+   # for charles' linux:
+   lilypond --ps Lahu_tune.ly
+fi
 echo Performing fixups
 ./fixups.sh
 #
@@ -30,15 +36,15 @@ echo Performing fixups
 cd tex
 rm -r *
 cp ../*.eps .
-cp ../$1 .
+cp ../$xml_file .
 cp ../*.tex .
 cp ../../transTeX/*.tex .
 cp ../*.bib .
 echo Generating LaTeX file "${texfile}.tex", timestamp: `date`
-xml_file=$1
+
 shift
-echo "python2 ../generateLaTeX.py $xml_file ../lahucatalog.tsv ../annotated_abbreviations.tsv ../triples.csv $which_part"
-python2 ../generateLaTeX.py $xml_file ../lahucatalog.tsv ../annotated_abbreviations.tsv ../triples.csv "$which_part"
+echo "python ../generateLaTeX.py $xml_file ../lahucatalog.tsv ../annotated_abbreviations.tsv ../triples.csv $which_part"
+/usr/bin/python ../generateLaTeX.py $xml_file ../lahucatalog.tsv ../annotated_abbreviations.tsv ../triples.csv "$which_part"
 
 sed -e '/% insert includes here/r./includes.tex' lahuTemplate.tex > ${texfile}.tex
 
