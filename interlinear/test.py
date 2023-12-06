d69628be59d5b72f4f74a16b611711f9e9bec848
John Lowe <johnblowe@gmail.com>
2023-12-06 13:36:05 -0800

    test.py now works with python3 to test transducers

--- interlinear/test.py
+++ interlinear/test.py
@@ -1,29 +1,33 @@
 #!usr/bin/env python
 
-import sys,os,codecs
+import sys, os, codecs
+import unicodedata
 import xml.etree.ElementTree as ET
-from Transducer import transduce,chinese,baptist,decompose
+from Transducer import transduce, chinese, baptist, decompose
+
 
 def main():
-#  for line in open('lahu_writing.txt','rU'):
-#    for word in line.split():
-#      print word,
-#      print transduce(word,decompose),
-#      print transduce(transduce(word,decompose),chinese),
-#      print transduce(transduce(word,decompose),baptist)
+    #  for line in open('lahu_writing.txt','rU'):
+    #    for word in line.split():
+    #      print word,
+    #      print transduce(word,decompose),
+    #      print transduce(transduce(word,decompose),chinese),
+    #      print transduce(transduce(word,decompose),baptist)
 
-  f = open('4testLahuTexts.xml','rt')
-  tree = ET.parse(f)
-  f.close()
-  for node in tree.findall('.//word/words/word/item'):
-    if node.attrib['type'] == 'txt':
-      form = codecs.encode(node.text,'utf8')
-      print form,
-      print [form],
-      print transduce(form,decompose),
-      print transduce(transduce(form,decompose),chinese),
-      print transduce(transduce(form,decompose),baptist)
+    f = open('4testLahuTexts.xml', 'r')
+    tree = ET.parse(f)
+    f.close()
+    for node in tree.findall('.//word/words/word/item'):
+        if node.attrib['type'] == 'txt':
+            # form = codecs.encode(node.text,'utf8')
+            form = node.text
+            print(form, end=' ')
+            # print([form], end=' ')
+            print(transduce(form, decompose), end=' ')
+            print(transduce(transduce(form, decompose), baptist), end=' ')
+            print(transduce(transduce(form, decompose), chinese), end=' ')
+            print()
 
 
 if __name__ == '__main__':
-  main()
+    main()
